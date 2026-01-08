246fff19cc231c25e1559987116e89cc4963f620
Andreas Fuchs <info@anfuchs.de>
2026-01-08 23:50:44 +0100

    fixes missing sensors

--- custom_components/wp_energy_predictor/__init__.py
+++ custom_components/wp_energy_predictor/__init__.py
@@ -1,12 +1,13 @@
-
 from .service import async_setup_services
 from .options_flow import WPEnergyPredictorOptionsFlow
 
 async def async_setup_entry(hass, entry):
     await async_setup_services(hass)
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass, entry):
+    await hass.config_entries.async_unload_platforms(entry, ["sensor"])
     return True
 
 def async_get_options_flow(config_entry):
